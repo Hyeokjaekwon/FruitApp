1944812541692fadba097fa4c474f94a2b65c116
KwonHyeokJae <21800041@handong.edu>
2021-09-12 21:28:25 +0900

    FristCommit

---
+++ src/com/example/lab3/Fruit.java
@@ -0,0 +1,50 @@
+package com.example.lab3;
+
+import java.util.Comparator;
+
+public class Fruit implements Comparator<Fruit>{
+	int no;
+	String name;
+	int price;
+	
+	Fruit(){};
+	
+	public Fruit(int no, String name, int price) {
+		this.no = no;
+		this.name = name;
+		this.price = price;
+	
+	}
+	@Override
+	public String toString() {
+		return "Fruit[no = " + no + ", name = "+ name +", price = " + price + "]";
+	}
+	
+	public int getNo() {
+		return no;
+	}
+	
+	public void setNo(int no) {
+		this.no = no;
+	
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setAge(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public int compare(Fruit o1, Fruit o2) {
+		return o1.name.compareTo(o2.name);
+	}
+	
+
+}
---
+++ src/com/example/lab3/Main.java
@@ -0,0 +1,43 @@
+package com.example.lab3;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Fruit> fl = new ArrayList<Fruit>();
+		System.out.println("fruit list (ordered by name)");
+		Fruit f1 = new Fruit(101, "Apple", 1000);
+		fl.add(f1);
+		Fruit f2 = new Fruit(102, "Watermelon", 1000);
+		fl.add(f2);
+		Fruit f3 = new Fruit(103, "Banana", 2000);
+		fl.add(f3);
+		Fruit f4 = new Fruit(104, "Melon", 1500);
+		fl.add(f4);
+		Fruit f5 = new Fruit(105, "Kiwi", 3000);
+		fl.add(f5);
+		Fruit f6 = new Fruit(106, "Tomato", 4000);
+		fl.add(f6);
+		Fruit f7 = new Fruit(107, "Orange", 2000);
+		fl.add(f7);
+		Fruit f8 = new Fruit(108, "Sweetberry", 1500);
+		fl.add(f8);
+		Fruit f9 = new Fruit(109, "Mango", 6000);
+		fl.add(f9);
+		Fruit f10 = new Fruit(101, "Durian", 1200);
+		fl.add(f10);
+		
+		Collections.sort(fl, new Fruit());
+		
+		for (Fruit s : fl) {
+			System.out.println(s.toString());
+		}
+		System.out.println("Fruit list reverse ordered by name");
+		Collections.sort(fl, new FruitComparatorDesc());
+		for (int i=0; i<fl.size(); i++) {
+			System.out.println(fl.get(i).toString());
+		}
+	}
+}
